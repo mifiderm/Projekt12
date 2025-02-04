be353fc17128b9f7b0907629ca8f25a3e6e871e6
Mia Fidermuc Vekić <mifiderm@vsite.hr>
2025-02-04 21:37:40 +0100

    21:37

--- IznajmljivanjeAutomobila/Models/CustomAuthenticationStateProvider.cs
+++ IznajmljivanjeAutomobila/Models/CustomAuthenticationStateProvider.cs
@@ -1,10 +1,9 @@
 ﻿using System.Security.Claims;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using Microsoft.EntityFrameworkCore;
-using BCrypt.Net;
 using IznajmljivanjeAutomobila.Data;
+using BCrypt.Net;
 using IznajmljivanjeAutomobila.Models;
 using System.Collections.Generic;
 
--- IznajmljivanjeAutomobila/Program.cs
+++ IznajmljivanjeAutomobila/Program.cs
@@ -3,8 +3,6 @@ using IznajmljivanjeAutomobila.Services;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using Microsoft.EntityFrameworkCore;
-using System;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,22 +14,29 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
-
 // Postavljanje AuthenticationStateProvider-a
 builder.Services.AddScoped<CustomAuthenticationStateProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 builder.Services.AddAuthorizationCore();
 
-
-
+// Postavljanje sesije
+builder.Services.AddScoped<ProtectedSessionStorage>();
 
 var app = builder.Build();
 
 // Automatska migracija baze
 using (var scope = app.Services.CreateScope())
 {
-    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    dbContext.Database.Migrate();
+    var authProvider = scope.ServiceProvider.GetRequiredService<CustomAuthenticationStateProvider>();
+    await authProvider.InitializeAsync(); // Pozivanje inicijalizacije odmah pri pokretanju aplikacije
+}
+
+
+// Inicijalizacija CustomAuthenticationStateProvider
+using (var scope = app.Services.CreateScope())
+{
+    var authProvider = scope.ServiceProvider.GetRequiredService<CustomAuthenticationStateProvider>();
+    await authProvider.InitializeAsync(); // Pozivanje inicijalizacije
 }
 
 // Middleware konfiguracija
